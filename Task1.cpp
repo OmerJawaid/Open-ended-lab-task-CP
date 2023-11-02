f43985cc734d0197f67f9f8d0a844cb78dc6d41e
OmerJawaid <omerjawaid0@gmail.com>
2023-11-02 22:49:02 +0500

    hello

--- Task1.cpp
+++ Task1.cpp
@@ -1,64 +1,64 @@
-#include<iostream>
-#include<string>
-using namespace std;
-void main()
-{
-	int books = 4, overdue_day;
-	char selection;
-	do
-	{
-		cout << "\nMenu" << endl
-			<< "If you want to Borrow press (B)" << endl
-			<< "If you want to Return press (R)" << endl
-			<< "If Quit press (Q)" << endl
-			<< "Enter your choice: ";
-		cin >> selection;
-		switch(selection)
-		{ 
-		case 'B':
-		{
-			if (books > 0)
-			{
-				books--;
-				cout << "Book Borrowed Successfully!"<<endl;
-			}
-			else
-				cout << "No Book is available at the moment."<<endl;
-			break;
-		}
-		case 'R':
-		{
-			if (books < 4)
-			{
-				int fine = 1;
-				books++;
-				cout << "Enter the number of days ";
-				cin >> overdue_day;
-				if (overdue_day > 5) {
-					fine = overdue_day * fine;
-					cout << "Your fine due to overdue book is " << fine << "$" << endl;
-					cout << "Book Returned Successfully" << endl;
-				}
-				else
-					cout << "Book Returned within due date." << endl
-					<< "Book returned Successfully"<<endl;
-			}
-			else
-				cout << "The book was not Issued"<<endl;
-			break;
-		}
-		case'q':
-		{
-
-		}
-		case 'Q':
-		{
-			cout << "Quit"<<endl;
-			break;
-		}
-		default:
-			cout << "Invalid Choice" << endl;
-			break;
-		}
-	} while (selection != 'Q' && selection != 'q');
-}
+//#include<iostream>
+//#include<string>
+//using namespace std;
+//void main()
+//{
+//	int books = 4, overdue_day;
+//	char selection;
+//	do
+//	{
+//		cout << "\nMenu" << endl
+//			<< "If you want to Borrow press (B)" << endl
+//			<< "If you want to Return press (R)" << endl
+//			<< "If Quit press (Q)" << endl
+//			<< "Enter your choice: ";
+//		cin >> selection;
+//		switch(selection)
+//		{ 
+//		case 'B':
+//		{
+//			if (books > 0)
+//			{
+//				books--;
+//				cout << "Book Borrowed Successfully!"<<endl;
+//			}
+//			else
+//				cout << "No Book is available at the moment."<<endl;
+//			break;
+//		}
+//		case 'R':
+//		{
+//			if (books < 4)
+//			{
+//				int fine = 1;
+//				books++;
+//				cout << "Enter the number of days ";
+//				cin >> overdue_day;
+//				if (overdue_day > 5) {
+//					fine = overdue_day * fine;
+//					cout << "Your fine due to overdue book is " << fine << "$" << endl;
+//					cout << "Book Returned Successfully" << endl;
+//				}
+//				else
+//					cout << "Book Returned within due date." << endl
+//					<< "Book returned Successfully"<<endl;
+//			}
+//			else
+//				cout << "The book was not Issued"<<endl;
+//			break;
+//		}
+//		case'q':
+//		{
+//
+//		}
+//		case 'Q':
+//		{
+//			cout << "Quit"<<endl;
+//			break;
+//		}
+//		default:
+//			cout << "Invalid Choice" << endl;
+//			break;
+//		}
+//	} while (selection != 'Q' && selection != 'q');
+//}
--- Task2.cpp
+++ Task2.cpp
@@ -2,49 +2,57 @@
 using namespace std;
 int main()
 {
-	int selection;
-	cout << "if you want to find prime numbers in a given range enter (1)"<<endl;
-	cout << "if you wanmt to check if a specific number is prime enter (2)"<<endl;
-	cin >> selection;
-	if (selection == 1)
+	char selection;
+	do
 	{
-		int limit,cont=0,n;
-		cout << "enter the range limit";
-		cin >> limit;
-		for (int i = 1; i <= limit; i++)
+		cout << "\nIf you want to find prime numbers in a given range enter (1)" << endl;
+		cout << "If you wanmt to check if a specific number is prime enter (2)" << endl;
+		cout << "Quit (Q)" << endl;
+		cin >> selection;
+		if (selection == '1')
 		{
-			for (int j = 1; j < i; j++)
+			int limit, cont = 0, n;
+			cout << "Enter the range limit: ";
+			cin >> limit;
+			for (int i = 1; i <= limit; i++)
 			{
-				if (i % j == 0)
+				for (int j = 1; j < i; j++)
 				{
-					cont++;
+					if (i % j == 0)
+					{
+						cont++;
+					}
 				}
-			}
-			if (cont == 1)
-			{
-				cout << i<<" ";
+				if (cont == 1)
+				{
+					cout << i << " ";
+					cont = 0;
+				}
 				cont = 0;
+				
 			}
-			cont = 0;
 		}
-	}
-	else if (selection == 2)
-	{
-		int number,cont=0;
-		cout << "input the number: ";
-		cin >> number;
-
-		for (int i = 1; i < number; i++)
+		else if (selection == '2')
 		{
-			if (number % i == 0)
-				cont++;
+			int number, cont = 0;
+			cout << "Input the number: ";
+			cin >> number;
+
+			for (int i = 1; i < number; i++)
+			{
+				if (number % i == 0)
+					cont++;
+			}
+
+			if (cont == 1)
+				cout << "It is a prime number."<<endl;
+			else
+				cout << "It is not a prime number."<<endl;
 		}
-		
-		if (cont == 1)
-			cout << "it is a prime number.";
 		else
-			cout << "it is not a prime number.";	
-	}
-	else
-		cout << "enter valid number.";
+		{
+			cout << "Enter valid number.";
+		}
+
+	} while (selection != 'q' && selection != 'Q');
 }
